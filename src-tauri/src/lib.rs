fbf94603e11c753e8c2ef8fc9e5cb9b6c10e9f02
ctmakes <8296195+ctmakes@users.noreply.github.com>
2025-12-26 19:36:58 +0800

    system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -43,9 +43,11 @@ pub fn run() {
 
             let menu = Menu::with_items(app, &[&open, &new_meeting, &settings, &exit])?;
 
-            let _tray = TrayIconBuilder::with_id("main-tray")
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .icon_as_template(true)
                 .menu(&menu)
-                .show_menu_on_left_click(false)
+                .show_menu_on_left_click(true)
                 .on_menu_event(|app, event| match event.id.as_ref() {
                     "open" => {
                         if let Some(window) = app.get_webview_window("main") {
@@ -71,24 +73,6 @@ pub fn run() {
                         app.exit(0);
                     }
                     _ => {}
-                })
-                .on_tray_icon_event(|tray, event| {
-                    if let tauri::tray::TrayIconEvent::Click {
-                        button: tauri::tray::MouseButton::Left,
-                        button_state: tauri::tray::MouseButtonState::Up,
-                        ..
-                    } = event
-                    {
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            if window.is_visible().unwrap_or(false) {
-                                let _ = window.hide();
-                            } else {
-                                let _ = window.show();
-                                let _ = window.set_focus();
-                            }
-                        }
-                    }
                 })
                 .build(app)?;
 
