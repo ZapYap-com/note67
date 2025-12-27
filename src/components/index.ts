00cfd595e3e884cc0610245c3fb307e681d6058f
ctmakes <8296195+ctmakes@users.noreply.github.com>
2025-12-27 17:50:42 +0800

    refactor settings component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { NoteEditor } from "./NoteEditor";
 export { NoteSearch } from "./NoteSearch";
 export { ModelManager } from "./ModelManager";
-export { Settings, useProfile } from "./Settings";
-export type { UserProfile } from "./Settings";
+export { Settings, useProfile } from "./settings";
+export type { UserProfile } from "./settings";
 export { SummaryPanel } from "./SummaryPanel";
 export { TranscriptViewer } from "./TranscriptViewer";
 export { TranscriptSearch } from "./TranscriptSearch";
